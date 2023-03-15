c4387eff8cd54e321e99d25a84bbe6f344aa5225
nguyenngocson <125906610+0962890418@users.noreply.github.com>
2023-03-15 22:37:32 +0700

    Add files via upload

---
+++ BaiThucHanhLab2/src/Bai2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Bai2{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap vao a: ");
+        int a = scanner.nextInt();
+        if(a%2==0)
+        {
+            System.out.println("Day la so chan~");
+        }
+        else{
+            System.out.println("Day la so le?");
+        }
+        scanner.close();
+    }
+}
+

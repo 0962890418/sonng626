807208cedaa47590b12f9ec4fbfddd23dd2b8bf4
nguyenngocson <125906610+0962890418@users.noreply.github.com>
2023-04-18 12:33:41 +0700

    Add files via upload

--- BaiThucHanhLab3/src/Main/App.java
+++ BaiThucHanhLab3/src/Main/App.java
@@ -1,51 +1,45 @@
 package Main;
-import src.Shapes.HinhChuNhat;
-import src.Shapes.HinhTron;
-import src.Shapes.HinhTruTron;
+
+import Shapes.HinhChuNhat;
+import Shapes.HinhTron;
+import Shapes.HinhTruTron;
+import Shapes.HinhVuong;
 
        public class App {
-       public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner (System. in);
-        String name;
-        System.out.printin(x: "Nhap hinh can su dung:
-        ");
-        name = scanner.nextLine();
-        I
-        switch (name) {
-        case "HinhTron":
-        HinhTron hinhTron = new HinhTron();
-        hinhTron.Ten();
-        hinhTron.nhapBanKinh();
-        hinhTron.tinhchuvi();
-        hinhTron.inChuvi();
-        hinhTron.tinhDienTich();
-        hinhTron.inDienTich();
-        break;
-        case "HinhTruTron":
-        hinhTruTron hinhTruTron = new HinhTru();
-        hinhTru.Ten();
-        hinhTru.nhapChieucao();
-        hinhTru.tinhTheTich();
-        hinhTru.inTheTich();
-        break;
-     case "HinhChuNhat":
-        HinhChuNhat hinhChuNhat = new HinhChuNhat () ;
-        hinhChuNhat.Ten();
-        hinhChuNhat.nhapChieuDai();
-        hinhChuNhat.nhapChieuRong(I;
-        hinhChuNhat.tinhChuvi();
-        hinhChuNhat.inChuvi();
-        hinhChuNhat.tinhDienTich();
-        hinhChuNhat.inDienTich();
-        break;
-        case "Hinhvuong":
-        HinhVuong hinhvuong = new Hinhvuong();
-        hinhvuong.Ten();
-        hinhvuong.nhapCanh();
-        hinhvuong.tinhChuvi();
-        hinhvuong.inChuvi();
-        hinhvuong.tinhDienTich();
-        hinhvuong-inDienTich();
-        break:
-        default:
-        System.out.println(x:
+       public static void main(String[] args) {
+   
+        HinhTron HinhTron = new HinhTron();
+        HinhTron.xuatTen();
+        HinhTron.NhapBanKinh();
+        HinhTron.TinhChuVi();
+        HinhTron.inChuVi();
+        HinhTron.TinhDienTich();
+        HinhTron.inDienTich();
+     
+        HinhTruTron HinhTruTron = new HinhTruTron();
+        HinhTruTron.Ten();
+        HinhTruTron.nhapBanKinh();
+        HinhTruTron.tinhTheTich();
+        HinhTruTron.inTheTich();
+ 
+        HinhChuNhat HinhChuNhat = new HinhChuNhat () ;
+        HinhChuNhat.Ten();
+        HinhChuNhat.nhapChieuDai();
+        HinhChuNhat.nhapChieuRong();
+        HinhChuNhat.tinhChuvi();
+        HinhChuNhat.inChuvi();
+        HinhChuNhat.tinhDienTich();
+        HinhChuNhat.inDienTich();
+  
+        HinhVuong HinhVuong = new HinhVuong();
+        HinhVuong.Ten();
+        HinhVuong.nhapCanh();
+        HinhVuong.tinhChuvi();
+        HinhVuong.inChuvi();
+        HinhVuong.tinhDienTich();
+        HinhVuong.inDienTich();
+      
+      }
+   }
+
+

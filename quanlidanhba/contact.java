95733258112d74caaec9615c0e1396815ded58fe
nguyenngocson <125906610+0962890418@users.noreply.github.com>
2023-05-16 13:48:43 +0700

    Delete quanlidanhba directory

--- quanlidanhba/contact.java
+++
@@ -1,8 +0,0 @@
-package quanlidanhba;
-
-public abstract class  contact {
-    abstract void insertContact(String name, String phone_number);
-    abstract void deleteContact(String name);
-    abstract void updateContact(String name, String new_number);
-    abstract void searchContact(String name);
-}

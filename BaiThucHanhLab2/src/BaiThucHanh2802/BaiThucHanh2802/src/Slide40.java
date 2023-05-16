c7d1976b9b019c543e359e95563b4dd01b290ddb
nguyenngocson <125906610+0962890418@users.noreply.github.com>
2023-05-16 13:50:57 +0700

    Add files via upload

---
+++ BaiThucHanhLab2/src/BaiThucHanh2802/BaiThucHanh2802/src/Slide40.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+ 
+public class Slide40 {
+ 
+    public static void main(String[] args) {
+        int month;
+        Scanner sc = new Scanner(System.in);
+         
+        System.out.println("Mời bạn nhập số: ");
+        day = sc.nextInt();
+         
+    switch (month) {
+        case 1:
+            System.out.println("Đây là tháng 1");
+            break;
+        case 2:
+            System.out.println("Đây là tháng 2");
+            break;
+        case 3:
+            System.out.println("Đây là tháng 3");
+            break;
+        case 4:
+            System.out.println("Đây là tháng 4");
+            break;
+        case 5:
+            System.out.println("Đây là thámg 5");
+            break;
+        case 6:
+            System.out.println("Đây là tháng 6");
+            break;
+        case 7:
+            System.out.println("Đây là thứ tháng 7");
+            break;
+        case 8:
+            System.out.println("Đây là thứ tháng 8");
+            break;    
+        case 9:
+            System.out.println("Đây là thứ tháng 9");
+            break; 
+        case 10:
+            System.out.println("Đây là thứ tháng 10");
+            break; 
+        case 11:
+            System.out.println("Đây là thứ tháng 11");
+            break;                  
+        case 12:
+            System.out.println("Đây là thứ tháng 12");
+            break;  
+        }
+        sc.close();
+    }
+}
+
+
